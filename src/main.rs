a2af2e2649fbfe682a3a5beb4bcb6bda33525888
Miles Sabin <miles@milessabin.com>
2025-11-02 14:54:11 +0000

    Added simple request logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 use axum::{Router, routing::get};
+use tower_http::LatencyUnit;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
+    // Initialize tracing with default level INFO
+    tracing_subscriber::fmt().with_max_level(Level::INFO).init();
+
     let app = Router::new()
         .route("/", get(root))
-        .route("/version", get(version));
+        .route("/version", get(version))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(
+                    DefaultMakeSpan::new()
+                        .level(Level::INFO)
+                        .include_headers(true),
+                )
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(LatencyUnit::Millis)
+                        .include_headers(true),
+                ),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
