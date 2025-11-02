772265053a7c31b117bd036bcfa5f597dd207b5b
Miles Sabin <miles@milessabin.com>
2025-11-02 12:47:59 +0000

    Added version route

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/version", get(version));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -14,4 +12,13 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello World!"
+}
+
+async fn version() -> &'static str {
+    built_info::GIT_VERSION.expect("GIT_VERSION should be set")
+}
+
+pub mod built_info {
+    // The file has been placed there by the build script.
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
